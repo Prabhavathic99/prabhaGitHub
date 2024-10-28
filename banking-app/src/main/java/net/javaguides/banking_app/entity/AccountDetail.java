78b8105914d2710c9ee532087640473259279a1c
Prabhavathic99 <prabhavathic99@gmail.com>
2024-10-28 19:16:11 +0530

    initial commit

---
+++ banking-app/src/main/java/net/javaguides/banking_app/entity/AccountDetail.java
@@ -0,0 +1,27 @@
+package net.javaguides.banking_app.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "bankTable")
+@Entity
+public class AccountDetail {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String accountNo;
+
+    @Column(name = "accountHolderName")
+    private String name;
+}
